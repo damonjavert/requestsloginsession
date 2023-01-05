d0157e165ace196d384356d50859ba80128321a4
DamonJavert <damonjavert@protonmail.com>
2023-01-05 20:50:25 +0800

    PEP styling changes

--- requestsloginsession/requestsloginsession.py
+++ requestsloginsession/requestsloginsession.py
@@ -15,7 +15,7 @@ logger = logging.getLogger('main.' + __name__)
 # loginData = {'username' : 'userstr', 'password' : 'passstr' }
 #
 # mywebsitesession = MyLoginSession(loginUrl, loginData, loginTestUrl, successStr)
-# resource = mywebsitesession.retrieveContent("https://website.com/cutedogpics")
+# resource = mywebsitesession.retrieve_content("https://website.com/cutedogpics")
 # print(resource.text)
 
 
@@ -72,31 +72,31 @@ class MyLoginSessionTest:
         t = os.path.getmtime(filename)
         return datetime.datetime.fromtimestamp(t)
 
-    def login(self, forceLogin=False, test_login=False, **kwargs):
+    def login(self, force_login=False, test_login=False, **kwargs):
         """
         login to a session. Try to read last saved session from cache file. If this fails
         do proper login. If the last cache access was too old, also perform a proper login.
         Always updates session cache file.
         """
-        wasReadFromCache = False
+        read_from_cache = False
         # logger.debug('loading or generating session...')
-        if os.path.exists(self.sessionFile) and not forceLogin:
+        if os.path.exists(self.sessionFile) and not force_login:
             time = self.modification_date(self.sessionFile)
 
-            # only load if file less than 30 minutes old
-            lastModification = (datetime.datetime.now() - time).seconds
-            if lastModification < self.maxSessionTime:
+            # only load if file less than maxSessionTimeSeconds old
+            last_modification = (datetime.datetime.now() - time).seconds
+            if last_modification < self.maxSessionTime:
                 with open(self.sessionFile, "rb") as f:
                     self.session = pickle.load(f)
-                    wasReadFromCache = True
-                    # logger.debug("loaded session from cache (last access %ds ago) " % lastModification)
-        if not wasReadFromCache:
+                    read_from_cache = True
+                    # logger.debug("loaded session from cache (last access %ds ago) " % last_modification)
+        if not read_from_cache:
             self.session = requests.Session()
             self.session.headers.update({'user-agent': self.userAgent})
             res = self.session.post(self.loginUrl, data=self.loginData,
                                     proxies=self.proxies, **kwargs)
             logger.debug('created new session with login')
-            self.saveSessionToCache()
+            self.save_session_to_cache()
 
         if test_login:
             logger.debug('Loaded session from cache and testing login...')
@@ -107,8 +107,7 @@ class MyLoginSessionTest:
             if 'Your username or password was incorrect.' in res.text:
                 raise Exception(f"Could not log into provided site {self.loginUrl}  - username or password was incorrect")
 
-
-    def saveSessionToCache(self):
+    def save_session_to_cache(self):
         """
         save session to a cache file
         """
@@ -117,7 +116,7 @@ class MyLoginSessionTest:
             pickle.dump(self.session, f)
             logger.debug('updated session cache-file %s' % self.sessionFile)
 
-    def retrieveContent(self, url, method="get", postData=None, postDataFiles=None, **kwargs):
+    def retrieve_content(self, url, method="get", post_data=None, post_data_files=None, **kwargs):
         """
         return the content of the url with respect to the session.
 
@@ -127,10 +126,10 @@ class MyLoginSessionTest:
         if method == 'get':
             res = self.session.get(url, proxies=self.proxies, **kwargs)
         else:
-            res = self.session.post(url, data=postData, proxies=self.proxies, files=postDataFiles, **kwargs)
+            res = self.session.post(url, data=post_data, proxies=self.proxies, files=post_data_files, **kwargs)
 
         # the session has been updated on the server, so also update in cache
-        self.saveSessionToCache()
+        self.save_session_to_cache()
 
         return res
 
